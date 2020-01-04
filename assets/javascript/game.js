39969e437c7b6d4853dcb4e83add462f898c728f
Zalina Magiday <zalina.ucf@gmail.com>
2020-01-04 15:05:19 -0500

    index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Setting for zero
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letterUser = [];
+var eachofLetters = null;
+
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+function farUserGuesses() {
+	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
+}
+
+countGuessesLeft();
+
+var restart = function() {
+	guessesLeft = 9;
+	letterUser = [];
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+
+document.onkeyup = function(event) {
+	guessesLeft--;
+
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	letterUser.push(userGuess);
+	countGuessesLeft();
+	farUserGuesses();
+
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose").innerHTML = "Loses: " + losses;
+		restart();
+	}
+  };
